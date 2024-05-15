117691130fa3a437d735cf7e722ca2834a6f6625
Muadh Adekunle Monsuru <muadh117a@gmail.com>
2024-05-15 16:07:47 +0100

    inital app skeleton setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
-
+import Main from './Components/Main';
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
-		<>
-			<h1>Love Gaming Dashboard</h1>
-		</>
+		<main className='font-ubuntu  h-screen overflow-hidden flex flex-col md:flex-row'>
+			<section className='w-full h-28 lg:w-1/4 lg:h-full bg-green-100 bg-mobile lg:bg-desktop bg-cover bg-no-repeat bg-center rounded-r-xl' />
+			<Main />
+		</main>
 	);
 }
 
---
+++ src/Components/Main.tsx
@@ -0,0 +1,11 @@
+export default function Main() {
+	return (
+		<section className='p-5 h-full w-full bg-red-100'>
+			<div className='p-1 border-b w-full'>
+				<h1 className='font-extrabold text-xl text-marine-blue'>
+					Love Gaming Management Dashboard
+				</h1>
+			</div>
+		</section>
+	);
+}
